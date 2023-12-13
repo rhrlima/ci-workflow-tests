ec25b7a966266ed5a0db834d3ae7363e563db025
Ricardo Lima <ricardo@brickabode.com>
2023-12-13 17:43:17 -0300

    feat: add main program

---
+++ src/myfsharpapp/Program.fs
@@ -0,0 +1,16 @@
+﻿let maxDepth (expression: string) =
+    expression
+    |> Seq.map (function | '(' -> 1 | ')' -> -1 | _ -> 0)
+    |> Seq.scan (+) 0
+    |> Seq.map abs
+    |> Seq.max
+
+[<EntryPoint>]
+let main argv =
+    if Array.length argv > 0 then
+        printfn "Expression: %s" argv.[0]
+        let result = maxDepth argv.[0]
+        printfn "Max Depth %d" result
+    else
+        printfn "Usage: program.exe \"(1+(2*3)+((8)/4))+1\""
+    0
