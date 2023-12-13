f093407aa9c7679f170597292b9a998ac18a13f5
Ricardo Lima <ricardo@brickabode.com>
2023-12-13 17:43:29 -0300

    feat: add tests project

---
+++ tests/myfshardpapp.test/Test.fs
@@ -0,0 +1,20 @@
+﻿namespace MyTests
+
+open NUnit.Framework
+open Program
+
+[<TestFixture>]
+type TestSuite() =
+  [<Test>]
+  member __.``maxDepth of () is 1``() =
+    let result = Program.maxDepth "()"
+    Assert.AreEqual(1, result)
+  
+  [<Test>]
+  member __.``maxDepth of (1+(2*3)+((8)/4))+1 is 3``() =
+    let result = Program.maxDepth "(1+(2*3)+((8)/4))+1"
+    Assert.AreEqual(3, result)
+
+  [<Test>]
+  member __.``true``() =
+    Assert.True(true)
